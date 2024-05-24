c732a29be73bb9bec64f09f664c2d5d7cd8f4797
alvaro <alvarobatrez27@gmail.com>
2024-05-23 19:08:14 -0600

    simple action server

---
+++ src/templates/scripts/simple_action_server.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+import rospy
+import actionlib
+from custom_msgs.msg import ExampleAction, ExampleResult, ExampleFeedback
+
+class TemplateActionServer():
+
+    def __init__(self):
+        self.action_server = actionlib.SimpleActionServer(
+            name='/example_action',
+            ActionSpec=ExampleAction,
+            execute_cb=self.execute_callback,
+            auto_start=False
+        )
+
+        self.action_server.start()
+
+        rospy.loginfo(msg='Action server is up')
+
+    def execute_callback(self, goal_handle):
+        rospy.loginfo(msg='Executing new goal')
+
+        goal = goal_handle.goal
+        result = ExampleResult()
+        feedback = ExampleFeedback()
+        success = False
+        cancel = False
+
+        rate = rospy.Rate(1.0)
+
+        counter = 0
+        max_num = 10
+
+        while True:
+
+            if self.action_server.is_preempt_requested():
+                cancel = True
+                break
+
+            if counter == goal:
+                success = True
+                break
+
+            if counter >= max_num:
+                break
+
+            counter += 1
+
+            feedback.feedback = counter
+            self.action_server.publish_feedback(feedback=feedback)
+
+            rate.sleep()
+
+        result.result = success
+
+        if cancel:
+            self.action_server.set_preempted(result=result)
+            rospy.logwarn(msg='Goal canceled')
+        elif success:
+            self.action_server.set_succeeded(result=result)
+            rospy.loginfo(msg='Goal succeeded')
+        else:
+            self.action_server.set_aborted(result=result)
+            rospy.logerr(msg='Goal aborted')
+
+if __name__ == '__main__':
+    rospy.init_node(name='action_server')
+    TemplateActionServer()
+    rospy.spin()
